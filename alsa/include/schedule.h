ab2616242925a519d95e7c921c15b0e3a249cc19
Jaroslav Kysela <perex@perex.cz>
1998-11-09 00:32:15 +0000

    Scheduler changes for 2.1.217...

--- alsa/include/schedule.h
+++ alsa/include/schedule.h
@@ -27,58 +27,87 @@
 #define SND_WK_NONE             0x00
 #define SND_WK_SLEEP            0x01	/* process is sleeping */
 
-#define snd_sleep_define( ident ) \
-  struct wait_queue *sleeper_##ident; \
-  unsigned long sleeper_end_jiffies_##ident; \
-  unsigned short sleeper_lock_##ident;
-#define snd_sleep_prepare( object, ident ) \
-  do { (object) -> sleeper_##ident = NULL; } while ( 0 )
-#define snd_sleep( object, ident, time ) \
-  do { \
-    (object) -> sleeper_end_jiffies_##ident = current -> timeout = jiffies + (time); \
-    interruptible_sleep_on( &(object) -> sleeper_##ident ); \
-  } while ( 0 )
-#define snd_sleep_u( object, ident, time ) \
-  do { \
-    (object) -> sleeper_end_jiffies_##ident = current -> timeout = jiffies + (time); \
-    sleep_on( &(object) -> sleeper_##ident ); \
-  } while ( 0 )
-#ifdef SND_POLL
-#define snd_poll_wait( file, object, ident, table ) \
-  poll_wait( file, &(object) -> sleeper_##ident, table )
-#else
-#define snd_select_wait( object, ident, table ) \
-  select_wait( &(object) -> sleeper_##ident, table );
-#endif
-#if LinuxVersionCode( 2, 1, 71 ) <= LINUX_VERSION_CODE
-#define snd_sleep_abort( object, ident ) \
-    ( signal_pending( current ) )
-#else
-#define snd_sleep_abort( object, ident ) \
-    ( current -> signal & ~current -> blocked )
-#endif
 #ifdef LINUX_2_1
+
+#define snd_sleep_define( ident ) \
+	struct wait_queue *sleeper_##ident; \
+	long sleeper_timeout_##ident; \
+	unsigned short sleeper_lock_##ident;
+#define snd_sleep_prepare( object, ident ) \
+	do { (object)->sleeper_##ident = NULL; \
+             (object)->sleeper_timeout_##ident = -1; } while ( 0 )
+#define snd_sleep( object, ident, time ) \
+	do { \
+    		(object)->sleeper_timeout_##ident = \
+      			interruptible_sleep_on_timeout( \
+      				&(object)->sleeper_##ident, time ); \
+  	} while ( 0 )
+#define snd_poll_wait( file, object, ident, table ) \
+	poll_wait( file, &(object)->sleeper_##ident, table )
+#define snd_sleep_abort( object, ident ) \
+	( signal_pending( current ) )
 #define snd_wakeup( object, ident ) \
-  do { \
-    wake_up( &(object) -> sleeper_##ident ); \
-    current -> need_resched = 1; \
-  } while ( 0 )
-#else
+	do { \
+		wake_up( &(object)->sleeper_##ident ); \
+		current->need_resched = 1; \
+	} while ( 0 )
+#define snd_getlock( object, ident ) (object)->sleeper_lock_##ident
+#define snd_timeout( object, ident ) \
+			( (object)->sleeper_timeout_##ident == 0 )
+#define snd_timevalue( object, ident ) (object)->sleeper_timeout_##ident
+#define snd_schedule( object, ident, time ) \
+	do { \
+		current->state = TASK_INTERRUPTIBLE; \
+		(object)->sleeper_timeout_##ident = schedule_timeout( time ); \
+	} while ( 0 )
+
+#else /* Linux 2.0.x */
+
+#define snd_sleep_define( ident ) \
+	struct wait_queue *sleeper_##ident; \
+	unsigned long sleeper_end_jiffies_##ident; \
+	unsigned short sleeper_lock_##ident;
+#define snd_sleep_prepare( object, ident ) \
+	do { (object)->sleeper_##ident = NULL; \
+	     (object)->sleeper_end_jiffies_##ident = -1; } while ( 0 )
+#define snd_sleep( object, ident, time ) \
+	do { \
+		(object)->sleeper_end_jiffies_##ident = \
+			current->timeout = jiffies + (time); \
+		interruptible_sleep_on( &(object)->sleeper_##ident ); \
+	} while ( 0 )
+#define snd_select_wait( object, ident, table ) \
+	select_wait( &(object)->sleeper_##ident, table );
+#define snd_sleep_abort( object, ident ) \
+	( current->signal & ~current->blocked )
 #define snd_wakeup( object, ident ) \
-  do { \
-    wake_up( &(object) -> sleeper_##ident ); \
-    need_resched = 1; \
-  } while ( 0 )
-#endif
-#define snd_getlock( object, ident ) (object) -> sleeper_lock_##ident
-#define snd_timeout( object, ident ) ( (object) -> sleeper_end_jiffies_##ident < jiffies )
-#define snd_timeout_value( object, ident ) ( (object) -> sleeper_end_jiffies_##ident - jiffies )
+	do { \
+		wake_up( &(object)->sleeper_##ident ); \
+		need_resched = 1; \
+	} while ( 0 )
+#define snd_getlock( object, ident ) (object)->sleeper_lock_##ident
+#define snd_timeout( object, ident ) \
+			( (object)->sleeper_end_jiffies_##ident < jiffies )
+#define snd_timevalue( object, ident ) \
+			( (signed long)((object)->sleeper_end_jiffies_##ident - jiffies) )
+#define snd_schedule( object, ident, time ) \
+	do { \
+		(object)->sleeper_end_jiffies_##ident = \
+			current->timeout = jiffies + (time); \
+		current->state = TASK_INTERRUPTIBLE; \
+		schedule(); \
+	} while ( 0 )
+
+#endif /* LINUX_2_1 */
+
+/* mutex */
+
 #define snd_mutex_define( ident ) \
   struct semaphore mutex_##ident;
 #define snd_mutex_prepare( object, ident ) \
-  (object) -> mutex_##ident = MUTEX;
-#define snd_mutex_down( object, ident ) down( &(object) -> mutex_##ident )
-#define snd_mutex_up( object, ident ) up( &(object) -> mutex_##ident )
+  (object)->mutex_##ident = MUTEX;
+#define snd_mutex_down( object, ident ) down( &(object)->mutex_##ident )
+#define snd_mutex_up( object, ident ) up( &(object)->mutex_##ident )
 #define snd_mutex_define_static( ident ) \
   static struct semaphore local_mutex_##ident = MUTEX;
 #define snd_mutex_down_static( ident ) down( &local_mutex_##ident )
