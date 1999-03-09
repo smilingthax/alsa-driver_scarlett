dfb6cd218da42a6d68ec21905ef390b5aed01595
Jaroslav Kysela <perex@perex.cz>
1999-03-09 15:49:33 +0000

    AC97 & spin lock fixes..

--- alsa/include/ac97_codec.h
+++ alsa/include/ac97_codec.h
@@ -71,6 +71,7 @@ struct snd_stru_ac97 {
 	void *private_data;
 	void (*private_free) (ac97_t * ac97);
 	/* --- */
+	int rev_is_not_rev;
 	unsigned int id;	/* identification of codec */
 	char name[64];		/* CODEC name */
 	unsigned short caps;	/* capabilities (register 0) */
