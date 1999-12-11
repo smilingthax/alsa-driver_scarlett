1bea7f04fe2d454f5f6594cf1a88716b6fd88960
Jaroslav Kysela <perex@perex.cz>
1999-12-11 20:18:06 +0000

    Uros Bizjak <uros@kss-loka.si> Sat, 11 Dec 1999 19:16:00 +0100 (CET) Also, I included (hopefully last) pack of tweaks and cleanups to CSP sources. Mostly sb16_csp.h cleanups and QSound handling tweaks.

--- alsa/include/sb16_csp.h
+++ alsa/include/sb16_csp.h
@@ -24,21 +24,36 @@
  */
 
 /* CSP modes */
-#define SND_SB_CSP_MODE_NONE            0x00
-#define SND_SB_CSP_MODE_DSP_READ        0x01	/* Record from DSP */
-#define SND_SB_CSP_MODE_DSP_WRITE       0x02	/* Play to DSP */
-#define SND_SB_CSP_MODE_QSOUND          0x04	/* QSound */
+#define SND_SB_CSP_MODE_NONE		0x00
+#define SND_SB_CSP_MODE_DSP_READ	0x01	/* Record from DSP */
+#define SND_SB_CSP_MODE_DSP_WRITE	0x02	/* Play to DSP */
+#define SND_SB_CSP_MODE_QSOUND		0x04	/* QSound */
+
+/* CSP load flags */
+#define SND_SB_CSP_LOAD_FROMUSER	0x01
+#define SND_SB_CSP_LOAD_INITBLOCK	0x02
 
 /* CSP sample width */
-#define SND_SB_CSP_SAMPLE_8BIT          0x01
-#define SND_SB_CSP_SAMPLE_16BIT         0x02
+#define SND_SB_CSP_SAMPLE_8BIT		0x01
+#define SND_SB_CSP_SAMPLE_16BIT		0x02
 
 /* CSP channels */
-#define SND_SB_CSP_MONO                 0x01
-#define SND_SB_CSP_STEREO               0x02
+#define SND_SB_CSP_MONO			0x01
+#define SND_SB_CSP_STEREO		0x02
 
-/* maximum size of micro code */
-#define SND_SB_CSP_MAX_MICROCODE_FILE_SIZE      0x3000
+/* CSP running state */
+#define SND_SB_CSP_ST_IDLE		0x00
+#define SND_SB_CSP_ST_LOADED		0x01
+#define SND_SB_CSP_ST_RUNNING		0x02
+#define SND_SB_CSP_ST_PAUSED		0x04
+#define SND_SB_CSP_ST_AUTO		0x08
+#define SND_SB_CSP_ST_QSOUND		0x10
+
+/* maximum QSound value (180 degrees right) */
+#define SND_SB_CSP_QSOUND_MAX		0x20
+
+/* maximum microcode file size */
+#define SND_SB_CSP_MAX_MICROCODE_FILE_SIZE	0x3000
 
 /* microcode header */
 typedef struct snd_sb_csp_mc_header {
@@ -58,61 +73,49 @@ typedef struct snd_sb_csp_start {
 	int channels;		/* channels, look above */
 } snd_sb_csp_start_t;
 
-/* running state */
-#define SND_SB_CSP_ST_IDLE      0x00
-#define SND_SB_CSP_ST_LOADED    0x01
-#define SND_SB_CSP_ST_RUNNING   0x02
-#define SND_SB_CSP_ST_PAUSED    0x04
-#define SND_SB_CSP_ST_AUTO      0x08
-#define SND_SB_CSP_ST_QSOUND    0x10
-
 /* CSP information */
 typedef struct snd_sb_csp_info {
-	char codec_name[16];	/* id name of codec */
-	unsigned short func_nr;	/* function number */
+	char codec_name[16];		/* id name of codec */
+	unsigned short func_nr;		/* function number */
 	unsigned int acc_format;	/* accepted PCM formats */
 	unsigned short acc_channels;	/* accepted channels */
 	unsigned short acc_width;	/* accepted sample width */
 	unsigned short csp_mode;	/* CSP mode, see above */
 	unsigned short run_channels;	/* current channels  */
 	unsigned short run_width;	/* current sample width */
-	unsigned short version;	/* version id: 0x10 - 0x1f */
-	unsigned short state;	/* state bits */
+	unsigned short version;		/* version id: 0x10 - 0x1f */
+	unsigned short state;		/* state bits */
 } snd_sb_csp_info_t;
 
-/* QSound positions */
+/* 33 QSound positions from left (0x00) to right (0x20) in 180 degree field */
 typedef struct snd_sb_csp_qsound {
-	/* 33 Qsound positions
-	 * from left (0x00) to right (0x20)
-	 * in 180 degree field
-	 */
-	unsigned char left;	/* left position */
-	unsigned char right;	/* right position */
+	unsigned char left;
+	unsigned char right;
 } snd_sb_csp_qsound_t;
 
 /* HWDEP controls */
 /* get CSP information */
-#define SND_SB_CSP_IOCTL_INFO           _IOR('H', 0x10, snd_sb_csp_info_t)
+#define SND_SB_CSP_IOCTL_INFO		_IOR('H', 0x10, snd_sb_csp_info_t)
 /* load microcode to CSP */
-#define SND_SB_CSP_IOCTL_LOAD_CODE      _IOW('H', 0x11, snd_sb_csp_microcode_t)
+#define SND_SB_CSP_IOCTL_LOAD_CODE	_IOW('H', 0x11, snd_sb_csp_microcode_t)
 /* unload microcode from CSP */
-#define SND_SB_CSP_IOCTL_UNLOAD_CODE    _IO('H', 0x12)
+#define SND_SB_CSP_IOCTL_UNLOAD_CODE	_IO('H', 0x12)
 /* start CSP */
-#define SND_SB_CSP_IOCTL_START          _IOW('H', 0x13, snd_sb_csp_start_t)
+#define SND_SB_CSP_IOCTL_START		_IOW('H', 0x13, snd_sb_csp_start_t)
 /* stop CSP */
-#define SND_SB_CSP_IOCTL_STOP           _IO('H', 0x14)
+#define SND_SB_CSP_IOCTL_STOP		_IO('H', 0x14)
 /* pause CSP and DMA transfer */
-#define SND_SB_CSP_IOCTL_PAUSE          _IO('H', 0x15)
+#define SND_SB_CSP_IOCTL_PAUSE		_IO('H', 0x15)
 /* restart CSP and DMA transfer */
-#define SND_SB_CSP_IOCTL_RESTART        _IO('H', 0x16)
+#define SND_SB_CSP_IOCTL_RESTART	_IO('H', 0x16)
 /* start QSound codec */
-#define SND_SB_CSP_IOCTL_QSOUND_SET_STATE _IOW('H', 0x20, int)
+#define SND_SB_CSP_IOCTL_QSOUND_SET_STATE	_IOW('H', 0x20, int)
 /* stop QSound codec */
-#define SND_SB_CSP_IOCTL_QSOUND_GET_STATE _IOR('H', 0x21, int)
+#define SND_SB_CSP_IOCTL_QSOUND_GET_STATE	_IOR('H', 0x21, int)
 /* set QSound channel positions */
-#define SND_SB_CSP_IOCTL_QSOUND_SET_POS _IOW('H', 0x22, snd_sb_csp_qsound_t)
+#define SND_SB_CSP_IOCTL_QSOUND_SET_POS	_IOW('H', 0x22, snd_sb_csp_qsound_t)
 /* get current QSound channel positions */
-#define SND_SB_CSP_IOCTL_QSOUND_GET_POS _IOR('H', 0x23, snd_sb_csp_qsound_t)
+#define SND_SB_CSP_IOCTL_QSOUND_GET_POS	_IOR('H', 0x23, snd_sb_csp_qsound_t)
 
 #ifdef __KERNEL__
 #include "sb.h"
@@ -146,11 +149,11 @@ typedef struct snd_sb_csp {
  * CSP call-backs
  */
 typedef struct {
-	int (*csp_use)(snd_sb_csp_t * p);
-	int (*csp_unuse)(snd_sb_csp_t * p);
-	int (*csp_autoload)(snd_sb_csp_t * p, int pcm_sfmt, int play_rec_mode);
-	int (*csp_start)(snd_sb_csp_t * p, int sample_width, int channels);
-	int (*csp_stop)(snd_sb_csp_t * p);
+	int (*csp_use) (snd_sb_csp_t * p);
+	int (*csp_unuse) (snd_sb_csp_t * p);
+	int (*csp_autoload) (snd_sb_csp_t * p, int pcm_sfmt, int play_rec_mode);
+	int (*csp_start) (snd_sb_csp_t * p, int sample_width, int channels);
+	int (*csp_stop) (snd_sb_csp_t * p);
 } snd_sb_csp_callback_t;
 
 #define SND_HWDEP_TYPE_SB16CSP  0x10	/* temporarily defined here */
