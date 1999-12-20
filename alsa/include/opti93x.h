48ce8445574cb8e6116a04ef30b5b2d3f0e0e678
Jaroslav Kysela <perex@perex.cz>
1999-12-20 11:31:09 +0000

    Massimo Piccioni <dafastidio@libero.it> Mon, 20 Dec 1999 01:37:27 +0100 the new opti92x/93x code

---
+++ alsa/include/opti93x.h
@@ -0,0 +1,185 @@
+#ifndef __OPTi93X_H
+#define __OPTi93X_H
+
+/*
+    opti93x.h - definitions for OPTi 82C93x chips.
+    Copyright (C) 1999 by Massimo Piccioni <piccio@caronte.csr.unibo.it>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "pcm.h"
+#include "mixer.h"
+#define OPTi93X
+#include "opti9xx.h"
+
+#define OPTi93X_INDEX			0x00
+#define OPTi93X_DATA			0x01
+#define OPTi93X_STATUS			0x02
+#define OPTi93X_DDATA			0x03
+#define OPTi93X_PORT(codec, r)		((codec)->port + OPTi93X_##r)
+
+#define OPTi93X_MIXOUT_LEFT		0x00
+#define OPTi93X_MIXOUT_RIGHT		0x01
+#define OPTi93X_CD_LEFT_INPUT		0x02
+#define OPTi93X_CD_RIGHT_INPUT		0x03
+#define OPTi930_AUX_LEFT_INPUT		0x04
+#define OPTi930_AUX_RIGHT_INPUT		0x05
+#define OPTi931_FM_LEFT_INPUT		0x04
+#define OPTi931_FM_RIGHT_INPUT		0x05
+#define OPTi93X_DAC_LEFT		0x06
+#define OPTi93X_DAC_RIGHT		0x07
+#define OPTi93X_PLAY_FORMAT		0x08
+#define OPTi93X_IFACE_CONF		0x09
+#define OPTi93X_PIN_CTRL		0x0a
+#define OPTi93X_ERR_INIT		0x0b
+#define OPTi93X_ID			0x0c
+#define OPTi93X_PLAY_UPR_CNT		0x0e
+#define OPTi93X_PLAY_LWR_CNT		0x0f
+#define OPTi931_AUX_LEFT_INPUT		0x10
+#define OPTi931_AUX_RIGHT_INPUT		0x11
+#define OPTi93X_LINE_LEFT_INPUT		0x12
+#define OPTi93X_LINE_RIGHT_INPUT	0x13
+#define OPTi93X_MIC_LEFT_INPUT		0x14
+#define OPTi93X_MIC_RIGHT_INPUT		0x15
+#define OPTi93X_OUT_LEFT		0x16
+#define OPTi93X_OUT_RIGHT		0x17
+#define OPTi93X_CAPT_FORMAT		0x1c
+#define OPTi93X_CAPT_UPR_CNT		0x1e
+#define OPTi93X_CAPT_LWR_CNT		0x1f
+
+#define OPTi93X_TRD			0x20
+#define OPTi93X_MCE			0x40
+#define OPTi93X_INIT			0x80
+
+#define OPTi93X_MIXOUT_MIC_GAIN		0x20
+#define OPTi93X_MIXOUT_LINE		0x00
+#define OPTi93X_MIXOUT_CD		0x40
+#define OPTi93X_MIXOUT_MIC		0x80
+#define OPTi93X_MIXOUT_MIXER		0xc0
+
+#define OPTi93X_STEREO			0x10
+#define OPTi93X_LINEAR_8		0x00
+#define OPTi93X_ULAW_8			0x20
+#define OPTi93X_LINEAR_16_LIT		0x40
+#define OPTi93X_ALAW_8			0x60
+#define OPTi93X_ADPCM_16		0xa0
+#define OPTi93X_LINEAR_16_BIG		0xc0
+
+#define OPTi93X_CAPTURE_PIO		0x80
+#define OPTi93X_PLAYBACK_PIO		0x40
+#define OPTi93X_AUTOCALIB		0x08
+#define OPTi93X_SINGLE_DMA		0x04
+#define OPTi93X_CAPTURE_ENABLE		0x02
+#define OPTi93X_PLAYBACK_ENABLE		0x01
+
+#define OPTi93X_IRQ_ENABLE		0x02
+
+#define OPTi93X_DMA_REQUEST		0x10
+#define OPTi93X_CALIB_IN_PROGRESS	0x20
+
+#define OPTi93X_IRQ_PLAYBACK		0x04
+#define OPTi93X_IRQ_CAPTURE		0x08
+
+
+typedef struct snd_stru_opti93x opti93x_t;
+struct snd_stru_opti93x {
+	unsigned short port;
+	unsigned short irq;
+	unsigned short dma1;
+	unsigned short dma2;
+	snd_dma_t *dma1ptr;
+	snd_dma_t *dma2ptr;
+
+	opti9xx_t *chip;
+	unsigned short hardware;
+	unsigned char image[32];
+
+	unsigned char mce_bit;
+	unsigned short mode;
+	int mute;
+
+	spinlock_t lock;
+
+	snd_card_t *card;
+	snd_pcm_t *pcm;
+	snd_pcm_subchn_t *playback_subchn;
+	snd_pcm_subchn_t *capture_subchn;
+	unsigned int p_dma_size;
+	unsigned int c_dma_size;
+
+	snd_kmixer_t *mixer;
+
+	snd_kmixer_element_t *me_mux;
+	snd_kmixer_element_t *me_accu;
+	snd_kmixer_element_t *me_dig_accu;
+
+	snd_kmixer_element_t *me_mux_cd;
+	snd_kmixer_element_t *me_mux_line;
+	snd_kmixer_element_t *me_mux_mic;
+	snd_kmixer_element_t *me_mux_mix;
+
+	snd_kmixer_element_t *me_in_aux;
+	snd_kmixer_element_t *me_in_cd;
+	snd_kmixer_element_t *me_in_fm;
+	snd_kmixer_element_t *me_in_line;
+	snd_kmixer_element_t *me_in_mic;
+
+	snd_kmixer_element_t *me_out_master;
+
+	snd_kmixer_element_t *me_sw_aux;
+	snd_kmixer_element_t *me_sw_cd;
+	snd_kmixer_element_t *me_sw_dac;
+	snd_kmixer_element_t *me_sw_fm;
+	snd_kmixer_element_t *me_sw_line;
+	snd_kmixer_element_t *me_sw_mgain;
+	snd_kmixer_element_t *me_sw_mic;
+	snd_kmixer_element_t *me_sw_out;
+
+	snd_kmixer_element_t *me_vol_aux;
+	snd_kmixer_element_t *me_vol_cd;
+	snd_kmixer_element_t *me_vol_dac;
+	snd_kmixer_element_t *me_vol_fm;
+	snd_kmixer_element_t *me_vol_igain;
+	snd_kmixer_element_t *me_vol_line;		
+	snd_kmixer_element_t *me_vol_mic;
+	snd_kmixer_element_t *me_vol_out;
+
+	snd_kmixer_element_t *me_adc;
+	snd_kmixer_element_t *me_dac;
+	snd_kmixer_element_t *me_capture;
+	snd_kmixer_element_t *me_playback;
+
+	unsigned int (*set_rate) (opti93x_t *codec, unsigned int rate);
+	void (*set_playback_format) (opti93x_t *codec, unsigned char format);
+	void (*set_capture_format) (opti93x_t *codec, unsigned char format);
+};
+
+#define OPTi93X_MODE_NONE	0x00
+#define OPTi93X_MODE_PLAY	0x01
+#define OPTi93X_MODE_CAPTURE	0x02
+#define OPTi93X_MODE_OPEN	(OPTi93X_MODE_PLAY | OPTi93X_MODE_CAPTURE)
+
+
+extern void snd_opti93x_interrupt(snd_pcm_t *pcm, unsigned char status);
+
+extern int snd_opti93x_new_pcm(snd_card_t *card, int device, opti9xx_t *chip,
+		snd_dma_t *dma1ptr, snd_dma_t *dma2ptr, snd_pcm_t **rpcm);
+
+extern int snd_opti93x_new_mixer(snd_pcm_t *pcm, int device,
+							snd_kmixer_t **rmixer);
+
+#endif	/* __OPTi93X_H */
+
---
+++ alsa/include/opti9xx.h
@@ -0,0 +1,379 @@
+
+#ifndef __OPTi9XX_H
+#define __OPTi9XX_H
+
+/*
+    opti9xx.h - definitions for OPTi 82c9xx chips.
+    Copyright (C) 1998-99 by Massimo Piccioni <piccio@caronte.csr.unibo.it>
+
+    Part of this code was developed at the Italian Ministry of Air Defence,
+    Sixth Division (oh, che pace ...), Rome.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#define OPTi9XX_HW_DETECT	0
+#define OPTi9XX_HW_82C928	1
+#define OPTi9XX_HW_82C929	2
+#define OPTi9XX_HW_82C924	3
+#define OPTi9XX_HW_82C925	4
+#define OPTi9XX_HW_82C930	5
+#define OPTi9XX_HW_82C931	6
+#define OPTi9XX_HW_82C933	7
+#define OPTi9XX_HW_LAST		OPTi9XX_HW_82C933
+
+#define OPTi9XX_MC_REG(n)	n
+
+struct snd_stru_opti9xx {
+	unsigned short hardware;
+	unsigned char password;
+	char name[7];
+
+	unsigned short mc_base;
+#ifdef OPTi93X
+	unsigned short mc_indir_index;
+#endif	/* OPTi93X */
+	unsigned short pwd_reg;
+	spinlock_t lock;
+
+	short wss_base;
+	short irq;
+	short dma1;
+#if defined(CS4231) || defined(OPTi93X)
+	short dma2;
+#endif	/* CS4231 || OPTi93X */
+	short fm_port;
+	short mpu_port;
+	short mpu_irq;
+};
+typedef struct snd_stru_opti9xx opti9xx_t;
+
+#define snd_opti9xx_printk(args...)	snd_printk(__FILE__": " ##args)
+
+
+extern inline void snd_opti9xx_free(opti9xx_t *chip)
+{
+	snd_kfree(chip);
+}
+
+extern opti9xx_t *snd_opti9xx_new_device(unsigned short hardware) {
+	opti9xx_t *chip;
+
+	if (!(chip = (opti9xx_t *) snd_kcalloc(sizeof(opti9xx_t), GFP_KERNEL)))
+		return NULL;
+
+	chip->hardware = hardware;
+	chip->lock = SPIN_LOCK_UNLOCKED;
+	chip->wss_base = chip->irq = chip->dma1 = -1;
+#if defined(CS4231) || defined (OPTi93X)
+	chip->dma2 = -1;
+#endif 	/* CS4231 || OPTi93X */
+	chip->fm_port = chip->mpu_port = chip->mpu_irq = -1;
+	switch (hardware) {
+#ifndef OPTi93X
+	case OPTi9XX_HW_82C928:
+	case OPTi9XX_HW_82C929:
+		strcpy(chip->name, 
+			(hardware == OPTi9XX_HW_82C928) ? "82C928" : "82C929");
+		chip->mc_base = 0xf8c;
+		chip->password = (hardware == OPTi9XX_HW_82C928) ? 0xe2 : 0xe3;
+		chip->pwd_reg = 3;
+		break;
+	case OPTi9XX_HW_82C924:
+	case OPTi9XX_HW_82C925:
+		strcpy(chip->name, 
+			(hardware == OPTi9XX_HW_82C924) ? "82C924" : "82C925");
+		chip->mc_base = 0xf8c;
+		chip->password = 0xe5;
+		chip->pwd_reg = 3;
+		break;
+#else
+	case OPTi9XX_HW_82C930:
+	case OPTi9XX_HW_82C931:
+	case OPTi9XX_HW_82C933:
+		strcpy(chip->name, 
+			(hardware == OPTi9XX_HW_82C930) ? "82C930" :
+			(hardware == OPTi9XX_HW_82C931) ? "82C931" : "82C933");
+		chip->mc_base = (hardware == OPTi9XX_HW_82C930) ? 0xf8f : 0xf8d;
+		chip->mc_indir_index = 0xe0e;
+		chip->password = 0xe4;
+		/* chip->pwd_reg = 0; */
+		break;
+#endif	/* OPTi93X */
+	default:
+		snd_opti9xx_printk("chip %d not supported.\n", hardware);
+		snd_opti9xx_free(chip);
+		chip = NULL;
+	}
+	return chip;
+}
+
+extern inline unsigned char snd_opti9xx_read(opti9xx_t *chip,
+							unsigned char reg) {
+	unsigned long flags;
+	unsigned char retval = 0xff;
+
+	spin_lock_irqsave(&chip->lock, flags);
+	outb(chip->password, chip->mc_base + chip->pwd_reg);
+	switch (chip->hardware) {
+#ifndef OPTi93X
+	case OPTi9XX_HW_82C924:
+	case OPTi9XX_HW_82C925:
+		if (reg > 7) {
+			outb(reg, chip->mc_base + 8);
+			outb(chip->password, chip->mc_base + chip->pwd_reg);
+			retval = inb(chip->mc_base + 9);
+			break;
+		}
+	case OPTi9XX_HW_82C928:
+	case OPTi9XX_HW_82C929:
+		retval = inb(chip->mc_base + reg);
+		break;
+#else
+	case OPTi9XX_HW_82C930:
+	case OPTi9XX_HW_82C931:
+	case OPTi9XX_HW_82C933:
+		outb(reg, chip->mc_indir_index);
+		outb(chip->password, chip->mc_base + chip->pwd_reg);
+		retval = inb(chip->mc_indir_index + 1);
+		break;
+#endif	/* OPTi93X */
+	default:
+		snd_opti9xx_printk("chip %d not supported.\n", chip->hardware);
+	}
+	spin_unlock_irqrestore(&chip->lock, flags);
+	return retval;
+}
+	
+extern inline void snd_opti9xx_write(opti9xx_t *chip, unsigned char reg,
+							unsigned char value) {
+	unsigned long flags;
+
+	spin_lock_irqsave(&chip->lock, flags);
+	outb(chip->password, chip->mc_base + chip->pwd_reg);
+	switch (chip->hardware) {
+#ifndef OPTi93X
+	case OPTi9XX_HW_82C924:
+	case OPTi9XX_HW_82C925:
+		if (reg > 7) {
+			outb(reg, chip->mc_base + 8);
+			outb(chip->password, chip->mc_base + chip->pwd_reg);
+			outb(value, chip->mc_base + 9);
+			break;
+		}
+	case OPTi9XX_HW_82C928:
+	case OPTi9XX_HW_82C929:
+		outb(value, chip->mc_base + reg);
+		break;
+#else
+	case OPTi9XX_HW_82C930:
+	case OPTi9XX_HW_82C931:
+	case OPTi9XX_HW_82C933:
+		outb(reg, chip->mc_indir_index);
+		outb(chip->password, chip->mc_base + chip->pwd_reg);
+		outb(value, chip->mc_indir_index + 1);
+		break;
+#endif	/* OPTi93X */
+	default:
+		snd_opti9xx_printk("chip %d not supported.\n", chip->hardware);
+	}
+	spin_unlock_irqrestore(&chip->lock, flags);
+}
+
+
+#define snd_opti9xx_mask(chip, reg, value, mask)	\
+	snd_opti9xx_write(chip, reg,			\
+		(snd_opti9xx_read(chip, reg) & ~(mask)) | ((value) & (mask)))
+
+extern int snd_opti9xx_configure(opti9xx_t *chip) {
+	unsigned char wss_base_bits;
+	unsigned char irq_bits;
+	unsigned char dma_bits;
+	unsigned char mpu_port_bits = 0;
+	unsigned char mpu_irq_bits;
+	unsigned long flags;
+
+	switch (chip->hardware) {
+#ifndef OPTi93X
+	case OPTi9XX_HW_82C924:
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(4), 0xf0, 0xfc);
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(6), 0x02, 0x02);
+	case OPTi9XX_HW_82C925:
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(1), 0x80, 0x80);
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(2), 0x00, 0x20);
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(3), 0xf0, 0xff);
+#ifdef CS4231
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(5), 0x02, 0x02);
+#else
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(5), 0x00, 0x02);
+#endif	/* CS4231 */
+		break;
+	case OPTi9XX_HW_82C928:
+	case OPTi9XX_HW_82C929:
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(1), 0x80, 0x80);
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(2), 0x00, 0x20);
+		/* snd_opti9xx_mask(chip, OPTi9XX_MC_REG(3), 0xa2, 0xae); */
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(4), 0x00, 0x0c);
+#ifdef CS4231
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(5), 0x02, 0x02);
+#else
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(5), 0x00, 0x02);
+#endif	/* CS4231 */
+		break;
+#else	/* OPTi93X */
+	case OPTi9XX_HW_82C930:
+	case OPTi9XX_HW_82C931:
+	case OPTi9XX_HW_82C933:
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(6), 0x02, 0x03);
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(3), 0x00, 0xff);
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(4), 0x10 |
+			(chip->hardware == OPTi9XX_HW_82C930 ? 0x00 : 0x04),
+			0x34);
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(5), 0x20, 0xbf);
+		break;
+#endif	/* OPTi93X */
+	default:
+		snd_opti9xx_printk("chip %d not supported.\n", chip->hardware);
+		return -EINVAL;
+	}
+
+	switch (chip->wss_base) {
+	case 0x530:
+		wss_base_bits = 0x00;
+		break;
+	case 0x604:
+		wss_base_bits = 0x03;
+		break;
+	case 0xe80:
+		wss_base_bits = 0x01;
+		break;
+	case 0xf40:
+		wss_base_bits = 0x02;
+		break;
+	default:
+		snd_opti9xx_printk("WSS port 0x%x not valid.\n",
+			chip->wss_base);
+		goto __skip_base;
+	}
+	snd_opti9xx_mask(chip, OPTi9XX_MC_REG(1), wss_base_bits << 4, 0x30);
+
+__skip_base:
+	switch (chip->irq) {
+#ifdef OPTi93X
+	case 5:
+		irq_bits = 0x05;
+		break;
+#endif	/* OPTi93X */
+	case 7:
+		irq_bits = 0x01;
+		break;
+	case 9:
+		irq_bits = 0x02;
+		break;
+	case 10:
+		irq_bits = 0x03;
+		break;
+	case 11:
+		irq_bits = 0x04;
+		break;
+	default:
+		snd_opti9xx_printk("WSS irq # %d not valid.\n", chip->irq);
+		goto __skip_resources;
+	}
+	switch (chip->dma1) {
+	case 0:
+		dma_bits = 0x01;
+		break;
+	case 1:
+		dma_bits = 0x02;
+		break;
+	case 3:
+		dma_bits = 0x03;
+		break;
+	default:
+		snd_opti9xx_printk("WSS dma1 # %d not valid.\n", chip->dma1);
+		goto __skip_resources;
+	}
+#if defined(CS4231) || defined(OPTi93X)
+	if (chip->dma1 == chip->dma2) {
+		snd_opti9xx_printk("don't want to share dmas.\n");
+		return -EBUSY;
+	}
+	switch (chip->dma2) {
+	case 0:
+	case 1:
+		break;
+	default:
+		snd_opti9xx_printk("WSS dma2 # %d not valid.\n", chip->dma2);
+		goto __skip_resources;
+	}
+	dma_bits |= 0x04;
+#endif	/* CS4231 || OPTi93X */
+	spin_lock_irqsave(&chip->lock, flags);
+	outb(irq_bits << 3 | dma_bits, chip->wss_base);
+	spin_unlock_irqrestore(&chip->lock, flags);
+
+__skip_resources:
+	if (chip->hardware > OPTi9XX_HW_82C928) {
+		switch (chip->mpu_port) {
+		case -1:
+			break;
+		case 0x300:
+			mpu_port_bits = 0x03;
+			break;
+		case 0x310:
+			mpu_port_bits = 0x02;
+			break;
+		case 0x320:
+			mpu_port_bits = 0x01;
+			break;
+		case 0x330:
+			mpu_port_bits = 0x00;
+			break;
+		default:
+			snd_opti9xx_printk("MPU-401 port 0x%x not valid.\n",
+				chip->mpu_port);
+			goto __skip_mpu;
+		}
+		switch (chip->mpu_irq) {
+		case 5:
+			mpu_irq_bits = 0x02;
+			break;
+		case 7:
+			mpu_irq_bits = 0x03;
+			break;
+		case 9:
+			mpu_irq_bits = 0x00;
+			break;
+		case 10:
+			mpu_irq_bits = 0x01;
+			break;
+		default:
+			snd_opti9xx_printk("MPU-401 irq # %d not valid.\n",
+				chip->mpu_irq);
+			goto __skip_mpu;
+		}
+		snd_opti9xx_mask(chip, OPTi9XX_MC_REG(6),
+			(chip->mpu_port == -1) ? 0x00 :
+				0x80 | mpu_port_bits << 5 | mpu_irq_bits << 3,
+			0xf8);
+	}
+__skip_mpu:
+
+	return 0;
+}
+
+#endif	/* __OPTi9XX_H */
+
