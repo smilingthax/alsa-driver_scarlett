70fb99329cb4154ba44cd9f0b53469a9e84b40ee
Jaroslav Kysela <perex@perex.cz>
2000-02-11 10:17:46 +0000

    Takashi Iwai <iwai@ww.uni-erlangen.de> The attached below is a patch to fix Oops in 2.3.x kernel and dependency for emu8000 and emu10k1 synths.

--- alsa/include/driver.h
+++ alsa/include/driver.h
@@ -150,6 +150,7 @@ static inline void snd_leave_user(mm_segment_t fs)
 typedef struct wait_queue wait_queue_t;
 typedef struct wait_queue * wait_queue_head_t;
 #define init_waitqueue_head(x) *(x) = NULL
+#define init_waitqueue_entry(q,p) ((q)->task = (p))
 #endif
 
 /*
