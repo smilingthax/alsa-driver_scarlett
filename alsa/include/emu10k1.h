5ca64044810d9026a73adb47868b24136fc102aa
Jaroslav Kysela <perex@perex.cz>
2000-08-21 14:40:44 +0000

    More updates according to Rui Sousa and EMU10K1 CVS (playback pointer)

--- alsa/include/emu10k1.h
+++ alsa/include/emu10k1.h
@@ -691,6 +691,7 @@ struct snd_stru_emu10k1_pcm {
 	int running;
 	snd_util_memblk_t *memblk;
 	unsigned int start_addr;
+	unsigned int ccca_start_addr;
 	unsigned int capture_inte;	/* interrupt enable mask */
 	unsigned int capture_ba_reg;	/* buffer address register */
 	unsigned int capture_bs_reg;	/* buffer size register */
