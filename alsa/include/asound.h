50e58f30d91ce218b38133d59371d3502c9e6846
Jaroslav Kysela <perex@perex.cz>
2000-10-03 08:35:17 +0000

    changed comment

--- alsa/include/asound.h
+++ alsa/include/asound.h
@@ -792,7 +792,7 @@ typedef struct snd_pcm_setup {
 	unsigned int msbits;		/* used most significant bits */
 	unsigned int rate_master;	/* Exact rate is rate_master / */
 	unsigned int rate_divisor;	/* rate_divisor */
-	size_t fifo_size;		/* chip FIFO size */
+	size_t fifo_size;		/* chip FIFO size in samples */
 	size_t transfer_block_size;	/* bus transfer block size */
 	char reserved[64];		/* must be filled with zero */
 } snd_pcm_setup_t;
