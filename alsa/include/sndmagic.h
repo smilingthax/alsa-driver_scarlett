c82bf04670323cfe0b15034b58744356fbe316c8
Takashi Iwai <tiwai@suse.de>
2000-12-04 16:59:25 +0000

    - Added an experimental Maestro3/Allegro driver.
    + Port of Zach's OSS driver.  Note that this driver is still experimental.
    + The driver works both playback and capture, but not duplex.
    + The mxier is sometimes not initialized properly.  Succeeding call
    of alsactl would initialize the mixer.

--- alsa/include/sndmagic.h
+++ alsa/include/sndmagic.h
@@ -136,6 +136,8 @@ static inline int _snd_magic_bad(void *obj, unsigned long magic)
 #define ymfpci_pcm_t_magic			0xa15a2f02
 #define cs4281_t_magic				0xa15a3001
 #define snd_i2c_bus_t_magic			0xa15a3101
+#define m3_t_magic				0xa15a3201
+#define m3_dma_t_magic				0xa15a3202
 
 #else
 #define snd_magic_kcalloc(type, extra, flags) (type *) snd_kcalloc(sizeof(type) + extra, flags)
