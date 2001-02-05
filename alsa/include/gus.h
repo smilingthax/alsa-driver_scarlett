b0679ed10c48558032ec492b28ed6d99f72d4538
Jaroslav Kysela <perex@perex.cz>
2001-02-05 15:03:10 +0000

    Various fixes inside the GUS PCM driver to support the latest midlevel code.

--- alsa/include/gus.h
+++ alsa/include/gus.h
@@ -462,6 +462,7 @@ struct _snd_gus_card {
 	snd_pcm_substream_t *pcm_cap_substream;
 	unsigned int c_dma_size;
 	unsigned int c_period_size;
+	unsigned int c_pos;
 
 	snd_rawmidi_t *midi_uart;
 	snd_rawmidi_substream_t *midi_substream_output;
