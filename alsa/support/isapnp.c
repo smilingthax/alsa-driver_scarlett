5941951d223400e5b86139e8776e43830383189f
Jaroslav Kysela <perex@perex.cz>
2001-09-24 08:55:13 +0000

    Added missing 8th value for DMA xtab

--- alsa/support/isapnp.c
+++ alsa/support/isapnp.c
@@ -1741,7 +1741,7 @@ static int isapnp_valid_dma(struct isapnp_cfgtmp *cfg, int idx)
 {
 	/* DMA priority: this table is good for i386 */
 	static unsigned short xtab[8] = {
-		1, 3, 5, 6, 7, 0, 2
+		1, 3, 5, 6, 7, 0, 2, 4
 	};
 	int err, i;
 	unsigned long *value1, *value2;
