8dcf331e33f99526c06fc4c871b84b4cf5c2ad8c
Jaroslav Kysela <perex@perex.cz>
2002-01-04 14:17:26 +0000

    Created include/adriver.h from include/sound/driver.h (separated support code for old kernels). Moved many declarations from driver.h and adriver.h to local files. Removed LINUX_2_3 definition (code must be compiled for 2.5 kernel by default).

--- alsa/include/compat_22.h
+++ alsa/include/compat_22.h
@@ -3,6 +3,13 @@
  */
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,3,0)
 
+#include <linux/list.h>
+#include <linux/kcomp.h>
+#undef IORESOURCE_IO /* bogus */
+#undef page_address  /* bogus */
+#include <linux/pagemap.h>
+#include <linux/ioport.h>
+
 #ifndef CONFIG_HAVE_DMA_ADDR_T
 typedef unsigned long dma_addr_t;
 #endif
@@ -73,11 +80,10 @@ static __inline__ void list_del_init(struct list_head *entry)
 	do { __restore_flags(flags); } while (0)
 
 /* Some distributions use modified kill_fasync */
-#ifdef CONFIG_OLD_KILL_FASYNC
-#define snd_kill_fasync(fp, sig, band) kill_fasync(*(fp), sig)
-#else
-#define snd_kill_fasync(fp, sig, band) kill_fasync(*(fp), sig, band)
-#endif
+#include <linux/fs.h>
+#undef kill_fasync
+#define kill_fasync(fp, sig, band) snd_wrapper_kill_fasync(fp, sig, band)
+void snd_wrapper_kill_fasync(struct fasync_struct **, int, int);
 
 #define tasklet_hi_schedule(t)	queue_task((t), &tq_immediate); \
 				mark_bh(IMMEDIATE_BH)
@@ -166,11 +172,12 @@ int snd_compat_release_resource(struct resource *resource);
 
 #define PCI_ANY_ID (~0)
 
-#define PCI_GET_DRIVER_DATA snd_pci_compat_get_driver_data
-#define PCI_SET_DRIVER_DATA snd_pci_compat_set_driver_data
+#define pci_get_drvdata snd_pci_compat_get_driver_data
+#define pci_set_drvdata snd_pci_compat_set_driver_data
 
 #define pci_set_dma_mask snd_pci_compat_set_dma_mask
 
+#undef pci_enable_device
 #define pci_enable_device snd_pci_compat_enable_device
 #define pci_register_driver snd_pci_compat_register_driver
 #define pci_unregister_driver snd_pci_compat_unregister_driver
@@ -186,12 +193,15 @@ int snd_compat_release_resource(struct resource *resource);
 #define pci_for_each_dev(dev) \
 	for(dev = pci_devices; dev; dev = dev->next)
 
+#undef pci_resource_start
 #define pci_resource_start(dev,bar) \
 	(((dev)->base_address[(bar)] & PCI_BASE_ADDRESS_SPACE) ? \
 	 ((dev)->base_address[(bar)] & PCI_BASE_ADDRESS_IO_MASK) : \
 	 ((dev)->base_address[(bar)] & PCI_BASE_ADDRESS_MEM_MASK))
+#undef pci_resource_end
 #define pci_resource_end(dev,bar) \
 	(pci_resource_start(dev,bar) + snd_pci_compat_get_size((dev),(bar)))
+#undef pci_resource_flags
 #define pci_resource_flags(dev,bar) (snd_pci_compat_get_flags((dev),(bar)))
 
 struct pci_device_id {
@@ -200,6 +210,10 @@ struct pci_device_id {
 	unsigned int class, class_mask;		/* (class,subclass,prog-if) triplet */
 	unsigned long driver_data;		/* Data private to the driver */
 };
+
+#ifndef PCI_OLD_SUSPEND
+#define PCI_OLD_SUSPEND
+#endif
 
 struct pci_driver {
 	struct list_head node;
@@ -293,6 +307,8 @@ enum
 	PM_SYS_VGA =	 0x41d00900, /* VGA controller */
 	PM_SYS_PCMCIA =	 0x41d00e00, /* PCMCIA controller */
 };
+
+#define __LINUX_PM_H	/* <linux/pm.h> in 2.2.18 is a bit stripped */
 
 /*
  * Device identifier
