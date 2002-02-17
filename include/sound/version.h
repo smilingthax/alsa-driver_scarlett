b4428029e3874f455450ea5a86189ae6845bd268
Jaroslav Kysela <perex@perex.cz>
2002-02-17 17:34:15 +0000

    0.9.0beta11

--- include/sound/version.h
+++ include/sound/version.h
@@ -1,3 +1,3 @@
 /* include/version.h.  Generated automatically by configure.  */
-#define CONFIG_SND_VERSION "0.9.0beta10"
-#define CONFIG_SND_DATE " (Sun Feb 17 09:08:45 2002 UTC)"
+#define CONFIG_SND_VERSION "0.9.0beta11"
+#define CONFIG_SND_DATE ""
