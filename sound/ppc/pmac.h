1f8ac9852a639c1ee525a7f712d1748acee8a3dd
Takashi Iwai <tiwai@suse.de>
2002-08-27 13:46:14 +0000

    the initial support for snapper (TAS3004) on some tibook.
    PCM, PCM1 and Monitor Mix volumes are added as stereo volumes.
    Only DCR threshold is selectable.  other DCR parameters are hardcoded.

--- sound/ppc/pmac.h
+++ sound/ppc/pmac.h
@@ -115,7 +115,7 @@ struct snd_pmac_beep {
  */
 
 enum snd_pmac_model {
-	PMAC_AWACS, PMAC_SCREAMER, PMAC_BURGUNDY, PMAC_DACA, PMAC_TUMBLER
+	PMAC_AWACS, PMAC_SCREAMER, PMAC_BURGUNDY, PMAC_DACA, PMAC_TUMBLER, PMAC_SNAPPER
 };
 
 struct snd_pmac {
