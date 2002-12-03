a5cc52d5ccaa97c4c2bbaffc283d186b22d853cf
Jaroslav Kysela <perex@perex.cz>
2002-12-03 17:53:43 +0000

    James Stafford <jstafford@ampltd.com> Added Envy24HT support...

--- sound/pci/ice1712/ice1712.h
+++ sound/pci/ice1712/ice1712.h
@@ -337,6 +337,7 @@ struct _snd_ice1712 {
 	snd_i2c_device_t *i2cdevs[2];	/* additional i2c devices */
 	
 	unsigned char gpio_direction, gpio_write_mask;
+	int vt1724;
 };
 
 #define chip_t ice1712_t
