22ccc0110de77f48e50ced3bb9872d26a42f251f
Takashi Iwai <tiwai@suse.de>
2003-02-19 09:21:46 +0000

    - fixed the slot bits of via823x for channels 4,5,6. - set SPDIF rate on via686 playback, too. some via686 devices have spdif connections.

--- sound/pci/via82xx.c
+++ sound/pci/via82xx.c
@@ -820,6 +820,7 @@ static int snd_via686_playback_prepare(snd_pcm_substream_t *substream)
 	snd_pcm_runtime_t *runtime = substream->runtime;
 
 	snd_ac97_set_rate(chip->ac97, AC97_PCM_FRONT_DAC_RATE, runtime->rate);
+	snd_ac97_set_rate(chip->ac97, AC97_SPDIF, runtime->rate);
 	via686_setup_format(chip, viadev, runtime);
 	return 0;
 }
@@ -919,8 +920,8 @@ static int snd_via8233_multi_prepare(snd_pcm_substream_t *substream)
 	case 2: slots = (1<<0) | (2<<4); break;
 	case 3: slots = (1<<0) | (2<<4) | (5<<8); break;
 	case 4: slots = (1<<0) | (2<<4) | (3<<8) | (4<<12); break;
-	case 5: slots = (1<<0) | (2<<4) | (5<<8) | (3<<12) | (4<<16); break;
-	case 6: slots = (1<<0) | (2<<4) | (5<<8) | (6<<12) | (3<<16) | (4<<20); break;
+	case 5: slots = (1<<0) | (2<<4) | (3<<8) | (4<<12) | (5<<16); break;
+	case 6: slots = (1<<0) | (2<<4) | (3<<8) | (4<<12) | (5<<16) | (6<<20); break;
 	default: slots = 0; break;
 	}
 	/* STOP index is never reached */
