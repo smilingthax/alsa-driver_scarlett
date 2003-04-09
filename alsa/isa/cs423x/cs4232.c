6866f95b2cc0e9c323a59abd2677f8d4800221e0
Jaroslav Kysela <perex@perex.cz>
2003-04-09 11:27:35 +0000

    Recoded cs423x and interwave drivers for new PnP layer

--- alsa/isa/cs423x/cs4232.c
+++ alsa/isa/cs423x/cs4232.c
@@ -1,1 +1,4 @@
 #include "../../alsa-kernel/isa/cs423x/cs4232.c"
+#ifndef __isapnp_now__
+#include "cs4232.isapnp"
+#endif
--- alsa/isa/cs423x/cs4236.c
+++ alsa/isa/cs423x/cs4236.c
@@ -1,2 +1,5 @@
 #include "../../alsa-kernel/isa/cs423x/cs4236.c"
+#ifndef __isapnp_now__
+#include "cs4236.isapnp"
+#endif
 EXPORT_NO_SYMBOLS;
