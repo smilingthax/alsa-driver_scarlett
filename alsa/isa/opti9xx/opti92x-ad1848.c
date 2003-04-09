27e1088eab1a01eaabeca89702356af7a4b1c22d
Jaroslav Kysela <perex@perex.cz>
2003-04-09 15:56:11 +0000

    Recoded ad1816a and opti drivers to new PnP layer

--- alsa/isa/opti9xx/opti92x-ad1848.c
+++ alsa/isa/opti9xx/opti92x-ad1848.c
@@ -1,3 +1,6 @@
 #define USE_OPL4
 #include "../../alsa-kernel/isa/opti9xx/opti92x-ad1848.c"
+#ifndef __isapnp_now__
+#include "opti92x-ad1848.isapnp"
+#endif
 EXPORT_NO_SYMBOLS;
--- alsa/isa/opti9xx/opti93x.c
+++ alsa/isa/opti9xx/opti93x.c
@@ -1,1 +1,4 @@
 #include "../../alsa-kernel/isa/opti9xx/opti93x.c"
+#ifndef __isapnp_now__
+#include "opti93x.isapnp"
+#endif
