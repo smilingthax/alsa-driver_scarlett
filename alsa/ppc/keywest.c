b144de367437fc37765e9d6f2210166a28b7e6ad
Jaroslav Kysela <perex@perex.cz>
2003-05-31 11:58:02 +0000

    2.5 cleanups

--- alsa/ppc/keywest.c
+++ alsa/ppc/keywest.c
@@ -2,6 +2,11 @@
 #include <linux/config.h>
 #include <linux/version.h>
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,3,0)
+#define CONFIG_ADB_CUDA
+#define CONFIG_ADB_PMU
+#endif
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2, 5, 0) /* FIXME: which version exactly? */
 #define i2c_device_name(x)	((x)->name)
 #define i2c_set_clientdata(x,p)	((x)->data = (p))
--- alsa/ppc/powermac.c
+++ alsa/ppc/powermac.c
@@ -1,5 +1,11 @@
 #include <linux/config.h>
 #include <linux/version.h>
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,3,0)
+#define CONFIG_ADB_CUDA
+#define CONFIG_ADB_PMU
+#endif
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2, 5, 0)
 #define PMAC_SUPPORT_PCM_BEEP
 #endif
