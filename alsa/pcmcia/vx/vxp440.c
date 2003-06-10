fe286b6d1a47764498bac1a89e176ef57da170af
Jaroslav Kysela <perex@perex.cz>
2003-06-10 14:04:26 +0000

    Added pcmcia compatible routines for 2.5

--- alsa/pcmcia/vx/vxp440.c
+++ alsa/pcmcia/vx/vxp440.c
@@ -1,2 +1,15 @@
+#include <linux/config.h>
+#include <linux/version.h>
+
+#if defined(CONFIG_MODVERSIONS) && !defined(__GENKSYMS__) && !defined(__DEPEND__)
+#define MODVERSIONS
+#include <linux/modversions.h>
+#include "sndversions.h"
+#endif
+
+#include "config.h"
+#include "adriver.h"
+#include "compat_cs.h"
+
 #include "../../alsa-kernel/pcmcia/vx/vxp440.c"
 EXPORT_NO_SYMBOLS;
