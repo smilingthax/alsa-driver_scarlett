35bfe11650fe1fe0c1b994454c86dba95d2f927a
Takashi Iwai <tiwai@suse.de>
2004-02-24 15:23:09 +0000

    - added snd-atiixp driver for ATI IXP 150/200/250 chips.

--- alsa/include/amagic.h
+++ alsa/include/amagic.h
@@ -7,5 +7,6 @@
 #define msndmidi_t_magic			0xa25a3e02
 #define snd_usX2Y_stream_t_magic                0xa25a5e03
 #define vortex_t_magic				0xa25a4001
+#define atiixp_t_magic				0xa25a5101
 
 #endif
