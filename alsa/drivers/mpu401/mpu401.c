56f47bae8c0563f7987bad5477a37134c15ed574
Clemens Ladisch <clemens@ladisch.de>
2004-03-16 11:16:29 +0000

    don't use acpi_disabled because it isn't exported in all archs

--- alsa/drivers/mpu401/mpu401.c
+++ alsa/drivers/mpu401/mpu401.c
@@ -1,12 +1,5 @@
 #include <linux/config.h>
 #include <linux/version.h>
 
-/* earlier kernels didn't export this variable */
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2,4,22)
-#ifdef CONFIG_ACPI_BUS
-static int acpi_disabled; /* dummy */
-#endif
-#endif
-
 #include "../../alsa-kernel/drivers/mpu401/mpu401.c"
 EXPORT_NO_SYMBOLS;
