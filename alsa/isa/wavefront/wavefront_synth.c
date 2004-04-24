ba5d0eea346a14524a84f0b0641690fa5cca9562
Jaroslav Kysela <perex@perex.cz>
2004-04-24 17:26:49 +0000

    Added syscalls.h support for 2.6 kernels (and provided compatible code for old kernels)

--- alsa/isa/wavefront/wavefront_synth.c
+++ alsa/isa/wavefront/wavefront_synth.c
@@ -1,2 +1,10 @@
 #define __NO_VERSION__
+
+#include <linux/config.h>
+#include <linux/version.h>
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,5)
+#define __KERNEL_SYSCALLS__
+#endif
+
 #include "../../alsa-kernel/isa/wavefront/wavefront_synth.c"
