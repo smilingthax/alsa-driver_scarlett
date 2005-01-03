f91b4641fb1f6acc59cac30ba28a794879e223a7
Takashi Iwai <tiwai@suse.de>
2005-01-03 11:49:08 +0000

    Hotplug firmware loader support
    Added hotplug firmware loader support.
    This feature is enabled only for 2.6 kernels, so far.
    The older kernels require still hdsploader.

--- alsa/pci/rme9652/hdsp.c
+++ alsa/pci/rme9652/hdsp.c
@@ -1,2 +1,7 @@
+#include <linux/config.h>
+#include <linux/version.h>
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,0)
+#define HDSP_USE_HWDEP_LOADER
+#endif
 #include "../../alsa-kernel/pci/rme9652/hdsp.c"
 EXPORT_NO_SYMBOLS;
