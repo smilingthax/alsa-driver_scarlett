56ab0028506076f54ba013424a599a4586a3e42d
Takashi Iwai <tiwai@suse.de>
2005-09-06 14:24:01 +0000

    Add a wrapper for register_sound_special_device()
    Add a check and a wrapper for register_sound_special_device().

--- alsa/acore/sound_oss.c
+++ alsa/acore/sound_oss.c
@@ -1,2 +1,6 @@
 #define __NO_VERSION__
+#include <sound/driver.h>
+#ifndef CONFIG_HAVE_REGISTER_SOUND_SPECIAL_DEVICE
+#define register_sound_special_device(ops,unit,dev) register_sound_special(ops,unit)
+#endif
 #include "../alsa-kernel/core/sound_oss.c"
