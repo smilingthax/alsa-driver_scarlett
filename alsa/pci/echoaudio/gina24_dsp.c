2ecf0e5ce73026caa73c16b9f90201d670d5b7ac
Giuliano Pochini <pochini@shiny.it>
2005-10-10 12:29:30 +0000

    Echoaudio, remove useless code
    - Remove unneeded calls to set_nominal_level() because the comm page has
    just been zeroed by init_dsp_comm_page().
    
    Signed-off-by: Giuliano Pochini <pochini@shiny.it>

--- alsa/pci/echoaudio/gina24_dsp.c
+++ alsa/pci/echoaudio/gina24_dsp.c
@@ -83,13 +83,6 @@ static int init_hw(echoaudio_t *chip, u16 device_id, u16 subdevice_id)
 	/* Must call this here after DSP is init to init gains and mutes */
 	err = init_line_levels(chip);
 
-	/* Set professional nominal levels (FALSE is +4dBu) */
-	for (i = 0; i < num_analog_busses_out(chip); i++)
-		err = set_nominal_level(chip, i, FALSE);
-
-	for (i = 0; i < num_analog_busses_in(chip); i++)
-		err = set_nominal_level(chip, bx_analog_in(chip) + i, FALSE);
-
 	/* Set the digital mode to S/PDIF RCA */
 	set_digital_mode(chip, DIGITAL_MODE_SPDIF_RCA);
 
