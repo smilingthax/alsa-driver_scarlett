ca3edf4c26e9992d88763dbb3a4a56ffe885f1e9
Martin Langer <martin-langer@gmx.de>
2006-04-24 14:51:52 +0200

    snd-miro include fix
    snd-miro isn't based on opti92x-ad1848.c. This patch fixes it by
    changing an include line.
    
    Signed-off-by: Martin Langer <martin-langer@gmx.de>

--- alsa/isa/opti9xx/miro.c
+++ alsa/isa/opti9xx/miro.c
@@ -1,6 +1,6 @@
 #include <linux/version.h>
 
-#include "../../alsa-kernel/isa/opti9xx/opti92x-ad1848.c"
+#include "../../alsa-kernel/isa/opti9xx/miro.c"
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,0)
 #ifndef __isapnp_now__
 #include "miro.isapnp"
