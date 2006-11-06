e9f3991641cb51b3b24f69b84a4699fef018c810
Clemens Ladisch <clemens@ladisch.de>
2006-11-06 09:28:03 +0100

    update list_for_each_entry() macro
    Update the list_for_each_entry() implementation for old kernels with the
    change made in 2.6.??.

--- alsa/include/adriver.h
+++ alsa/include/adriver.h
@@ -133,11 +133,9 @@ typedef __u32 __be32;
 #endif
 #ifndef list_for_each_entry
 #define list_for_each_entry(pos, head, member)                          \
-        for (pos = list_entry((head)->next, typeof(*pos), member),      \
-                     prefetch(pos->member.next);                        \
-             &pos->member != (head);                                    \
-             pos = list_entry(pos->member.next, typeof(*pos), member),  \
-                     prefetch(pos->member.next))
+	for (pos = list_entry((head)->next, typeof(*pos), member);	\
+	     prefetch(pos->member.next), &pos->member != (head);	\
+	     pos = list_entry(pos->member.next, typeof(*pos), member))
 #endif
 #ifndef list_for_each_entry_safe
 #define list_for_each_entry_safe(pos, n, head, member)			\
