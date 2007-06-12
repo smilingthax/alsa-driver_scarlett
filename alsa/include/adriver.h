cafc770ec8712ded39a158604eb2900ef0c16a6c
Takashi Iwai <tiwai@suse.de>
2007-06-12 14:17:25 +0200

    Fix the wrong kfree error with kmemdup
    We forgot a wrapper for kmemdup with memory debug option.
    It resulted in the wrong kfree message (and memory leaks).

--- alsa/include/adriver.h
+++ alsa/include/adriver.h
@@ -1362,7 +1362,7 @@ static inline int snd_pci_enable_msi(struct pci_dev *dev) { return -1; }
 #endif
 
 /* kmemdup() wrapper */
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 19)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 19) || defined(CONFIG_SND_DEBUG_MEMORY)
 #include <linux/string.h>
 static inline void *snd_kmemdup(const void *src, size_t len, gfp_t gfp)
 {
