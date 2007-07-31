d4a17772c8c3e8b4270c8711eeab0a63a7413746
Timur Tabi <timur@freescale.com>
2007-07-31 18:18:44 +0200

    ASoC CS4270 codec device driver
    This patch adds ALSA SoC support for the Cirrus Logic CS4270 codec.  The
    following features are suppored:
    
    1) Stand-alone and software mode
    2) Software mode via I2C only
    3) Master mode, not Slave
    4) No power management
    
    Signed-off-by: Timur Tabi <timur@freescale.com>

--- sound/kernel/include/linux/i2c-id.h
+++ sound/kernel/include/linux/i2c-id.h
@@ -120,6 +120,7 @@
 #define I2C_DRIVERID_WM8750	90	/* Wolfson WM8750 audio codec */
 #define I2C_DRIVERID_WM8753	91	/* Wolfson WM8753 audio codec */
 #define I2C_DRIVERID_LM4857 	92 	/* LM4857 Audio Amplifier */
+#define I2C_DRIVERID_CS4270	93	/* Cirrus Logic 4270 audio codec */
 
 #define I2C_DRIVERID_I2CDEV	900
 #define I2C_DRIVERID_ARP        902    /* SMBus ARP Client              */
