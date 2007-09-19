12dfc4751a6c76ca1a4e0057129a7abc4b9854cc
Stas Sergeev <stsp@aknet.ru>
2007-09-19 21:26:21 +0200

    PC-Speaker driver update
    - PCSP_HOOK is no longer needed, removed.
    The driver fully moved to hrtimers and the kernel patch is now optional.
    - Depend on EXPERIMENTAL in preparation to submit this upstream.
    - Remove the built-in software volume control in favour of softvol.
    
    Signed-off-by: Stas Sergeev <stsp@aknet.ru>

--- alsa/drivers/pcsp/pcsp_mixer.c
+++ alsa/drivers/pcsp/pcsp_mixer.c
@@ -8,56 +8,8 @@
 #include <sound/driver.h>
 #include <sound/core.h>
 #include <sound/control.h>
-#include "pcsp_tabs.h"
 #include "pcsp.h"
 
-/*
-   calculate a translation-table for PC-Speaker
-*/
-void pcsp_calc_voltab(struct snd_pcsp *chip)
-{
-	int i;
-	unsigned int j;
-	spin_lock_irq(&chip->vl_lock);
-	for (i = 0; i < 256; i++) {
-		j = (((pcsp_tabs[chip->gain][i] - 128)
-		      * (signed)chip->volume) / PCSP_MAX_VOLUME) + 128;
-		chip->vl_tab[i] = j * CUR_DIV / 256;
-	}
-	spin_unlock_irq(&chip->vl_lock);
-}
-
-static int pcsp_volume_info(struct snd_kcontrol *kcontrol,
-			    struct snd_ctl_elem_info *uinfo)
-{
-	uinfo->type = SNDRV_CTL_ELEM_TYPE_INTEGER;
-	uinfo->count = 1;
-	uinfo->value.integer.min = 0;
-	uinfo->value.integer.max = PCSP_MAX_VOLUME;
-	return 0;
-}
-
-static int pcsp_volume_get(struct snd_kcontrol *kcontrol,
-			   struct snd_ctl_elem_value *ucontrol)
-{
-	struct snd_pcsp *chip = snd_kcontrol_chip(kcontrol);
-	ucontrol->value.integer.value[0] = chip->volume;
-	return 0;
-}
-
-static int pcsp_volume_put(struct snd_kcontrol *kcontrol,
-			   struct snd_ctl_elem_value *ucontrol)
-{
-	struct snd_pcsp *chip = snd_kcontrol_chip(kcontrol);
-	int changed = 0;
-	int vol = ucontrol->value.integer.value[0];
-	if (vol != chip->volume) {
-		chip->volume = vol;
-		pcsp_calc_voltab(chip);
-		changed = 1;
-	}
-	return changed;
-}
 
 static int pcsp_enable_info(struct snd_kcontrol *kcontrol,
 			    struct snd_ctl_elem_info *uinfo)
@@ -90,38 +42,6 @@ static int pcsp_enable_put(struct snd_kcontrol *kcontrol,
 	return changed;
 }
 
-static int pcsp_gain_info(struct snd_kcontrol *kcontrol,
-			  struct snd_ctl_elem_info *uinfo)
-{
-	uinfo->type = SNDRV_CTL_ELEM_TYPE_INTEGER;
-	uinfo->count = 1;
-	uinfo->value.integer.min = 0;
-	uinfo->value.integer.max = pcsp_max_gain;
-	return 0;
-}
-
-static int pcsp_gain_get(struct snd_kcontrol *kcontrol,
-			 struct snd_ctl_elem_value *ucontrol)
-{
-	struct snd_pcsp *chip = snd_kcontrol_chip(kcontrol);
-	ucontrol->value.integer.value[0] = chip->gain;
-	return 0;
-}
-
-static int pcsp_gain_put(struct snd_kcontrol *kcontrol,
-			 struct snd_ctl_elem_value *ucontrol)
-{
-	struct snd_pcsp *chip = snd_kcontrol_chip(kcontrol);
-	int changed = 0;
-	int gain = ucontrol->value.integer.value[0];
-	if (gain != chip->gain) {
-		chip->gain = gain;
-		pcsp_calc_voltab(chip);
-		changed = 1;
-	}
-	return changed;
-}
-
 static int pcsp_treble_info(struct snd_kcontrol *kcontrol,
 			    struct snd_ctl_elem_info *uinfo)
 {
@@ -131,7 +51,7 @@ static int pcsp_treble_info(struct snd_kcontrol *kcontrol,
 	uinfo->value.enumerated.items = chip->max_treble + 1;
 	if (uinfo->value.enumerated.item > chip->max_treble)
 		uinfo->value.enumerated.item = chip->max_treble;
-	sprintf(uinfo->value.enumerated.name, "%d", PCSP_RATE);
+	sprintf(uinfo->value.enumerated.name, "%d", PCSP_RATE());
 	return 0;
 }
 
@@ -154,7 +74,6 @@ static int pcsp_treble_put(struct snd_kcontrol *kcontrol,
 #if PCSP_DEBUG
 		printk(KERN_INFO "PCSP: rate set to %i\n", PCSP_RATE);
 #endif
-		pcsp_calc_voltab(chip);
 		changed = 1;
 	}
 	return changed;
@@ -201,9 +120,7 @@ static int pcsp_pcspkr_put(struct snd_kcontrol *kcontrol,
 }
 
 static struct snd_kcontrol_new __initdata snd_pcsp_controls[] = {
-	PCSP_MIXER_CONTROL(volume, "Master Playback Volume"),
 	PCSP_MIXER_CONTROL(enable, "Master Playback Switch"),
-	PCSP_MIXER_CONTROL(gain, "PCM Playback Volume"),
 	PCSP_MIXER_CONTROL(treble, "BaseFRQ Playback Volume"),
 	PCSP_MIXER_CONTROL(pcspkr, "PC Speaker Playback Switch"),
 };
