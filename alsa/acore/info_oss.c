423c1ca68cf777e57a5da4e369ece703b11da1c1
Clemens Ladisch <clemens@ladisch.de>
2008-01-22 08:35:03 +0100

    fix inclusion of adriver.h
    Add or move the inclusion of "adriver.h" to the proper places in some
    build stubs.

--- alsa/acore/info_oss.c
+++ alsa/acore/info_oss.c
@@ -1,7 +1,7 @@
 #define __NO_VERSION__
+#include "adriver.h"
 #include <linux/version.h>
 #ifndef CONFIG_HAVE_INIT_UTSNAME
 #define init_utsname()	(&system_utsname)
 #endif
-#include "adriver.h"
 #include "../alsa-kernel/core/info_oss.c"
