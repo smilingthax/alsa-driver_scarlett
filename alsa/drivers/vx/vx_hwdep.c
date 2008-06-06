439e707f827320b48f078d2214a6e6b43c2e9413
Jaroslav Kysela <perex@t61.perex-int.cz>
2008-06-06 10:29:13 +0200

    configure: Added CONFIG_HAVE_DEPRECATED_CONFIG_H and CONFIG_HAVE_IS_POWER_OF_2 checks
    Also all files using config.h directly were modified to use autoconf.h
    conditionaly.
    
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- alsa/drivers/vx/vx_hwdep.c
+++ alsa/drivers/vx/vx_hwdep.c
@@ -1,6 +1,11 @@
+#include "config.h"
 #define __NO_VERSION__
 /* to be in alsa-driver-specfici code */
+#ifdef CONFIG_HAVE_DEPRECATED_CONFIG_H
+#include <linux/autoconf.h>
+#else
 #include <linux/config.h>
+#endif
 #include <linux/version.h>
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,0)
 #define CONFIG_USE_VXLOADER
--- alsa/drivers/vx/vx_pcm.c
+++ alsa/drivers/vx/vx_pcm.c
@@ -1,6 +1,11 @@
+#include "config.h"
 #define __NO_VERSION__
 /* to be in alsa-driver-specfici code */
+#ifdef CONFIG_HAVE_DEPRECATED_CONFIG_H
+#include <linux/autoconf.h>
+#else
 #include <linux/config.h>
+#endif
 #include <linux/version.h>
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2, 2, 18)
 #define vmalloc_32(x) vmalloc_nocheck(x)
