9ee06f269d1edd0bcefbe53cab650e38d4047c50
Takashi Iwai <tiwai@suse.de>
2008-10-16 16:11:55 +0200

    Add missing jack.c build stub
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/acore/jack.c
+++ alsa/acore/jack.c
@@ -1,7 +1,7 @@
 #define __NO_VERSION__
 #include "adriver.h"
-#include "input.h"
+#include <linux/input.h>
 #ifndef SW_LINEOUT_INSERT
-#define SW_LINEOUT_INSERT 0x06
+#define SW_LINEOUT_INSERT	0x06  /* set = inserted */
 #endif
 #include "../alsa-kernel/core/jack.c"
