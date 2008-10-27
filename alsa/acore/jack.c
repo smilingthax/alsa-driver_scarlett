637f794de66fdf32920a895fb3ba631d857e6479
Takashi Iwai <tiwai@suse.de>
2008-10-27 18:50:41 +0100

    jack - fix build with older kernels
    Add a definition of SW_LINEOUT_INSERT for older kernels.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/acore/jack.c
+++ alsa/acore/jack.c
@@ -1,3 +1,7 @@
 #define __NO_VERSION__
 #include "adriver.h"
+#include <linux/input.h>
+#ifndef SW_LINEOUT_INSERT
+#define SW_LINEOUT_INSERT	0x06  /* set = inserted */
+#endif
 #include "../alsa-kernel/core/jack.c"
