384b9fa3752f248022c8ac497084a22b075f9f27
Takashi Iwai <tiwai@suse.de>
2008-10-30 20:24:48 +0100

    soc - Fix build with 2.6.25 or earler kernel
    Simply force to undefine CONFIG_DEBUG_FS since debugfs_remove_recursive()
    doesn't exist in the earlier kernels.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/soc/soc-core.c
+++ alsa/soc/soc-core.c
@@ -1,2 +1,6 @@
 #include "adriver.h"
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 26)
+/* HACK HACK - debugfs_remove_recursive() isn't defined */
+#undef CONFIG_DEBUG_FS
+#endif
 #include "../alsa-kernel/soc/soc-core.c"
