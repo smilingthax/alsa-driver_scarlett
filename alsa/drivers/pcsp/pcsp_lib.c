5a596b99cf3d0754231a4de23e741d6568813f60
Takashi Iwai <tiwai@suse.de>
2008-11-26 14:25:31 +0100

    Add a wrapper for ns_to_ktime() for drivers/pcsp/pcsp_lib.c
    Simply use ktime_set(), assuming it's 32bit.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/drivers/pcsp/pcsp_lib.c
+++ alsa/drivers/pcsp/pcsp_lib.c
@@ -2,4 +2,7 @@
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 28)
 #define hrtimer_get_expires(t)	(t)->expires
 #endif
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 27)
+#define ns_to_ktime(ns)		ktime_set(0, ns)
+#endif
 #include "../../alsa-kernel/drivers/pcsp/pcsp_lib.c"
