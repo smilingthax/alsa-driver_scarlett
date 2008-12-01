3202c9206163e06a641071ca0377defa08fdb7b2
Takashi Iwai <tiwai@suse.de>
2008-12-01 21:28:47 +0100

    Fix pcsp.c for 2.6.26 or older kernels
    HRTIMER_CG_IRQSAFE_UNLOCKED is used rather in this code...
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/drivers/pcsp/pcsp.c
+++ alsa/drivers/pcsp/pcsp.c
@@ -1,2 +1,5 @@
 #include "adriver.h"
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 27)
+#define HRTIMER_CB_IRQSAFE_UNLOCKED	HRTIMER_CB_IRQSAFE
+#endif
 #include "../../alsa-kernel/drivers/pcsp/pcsp.c"
