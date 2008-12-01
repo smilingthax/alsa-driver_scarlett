75916d18ff254f2a4f15fe66c85b4bd79e148a10
Takashi Iwai <tiwai@suse.de>
2008-12-01 07:48:12 +0100

    Add HRTIMER_CB_IRQSAFE_UNLOCKED wrapper for 2.6.26 and older for pcsp
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/drivers/pcsp/pcsp_lib.c
+++ alsa/drivers/pcsp/pcsp_lib.c
@@ -4,5 +4,6 @@
 #endif
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 27)
 #define ns_to_ktime(ns)		ktime_set(0, ns)
+#define HRTIMER_CB_IRQSAFE_UNLOCKED	HRTIMER_CB_IRQSAFE
 #endif
 #include "../../alsa-kernel/drivers/pcsp/pcsp_lib.c"
