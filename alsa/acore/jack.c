a4bce78a62960b9feda33cb401c72f162a34ede3
Takashi Iwai <tiwai@suse.de>
2008-12-10 19:55:20 +0100

    Define SW_JACK_PHYSICAL_INSERT for jack.c
    Define SW_JACK_PHYSICAL_INSERT for jack.c when not defined in the kernel yet.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/acore/jack.c
+++ alsa/acore/jack.c
@@ -4,4 +4,7 @@
 #ifndef SW_LINEOUT_INSERT
 #define SW_LINEOUT_INSERT	0x06  /* set = inserted */
 #endif
+#ifndef SW_JACK_PHYSICAL_INSERT
+#define SW_JACK_PHYSICAL_INSERT 0x07  /* set = mechanical switch set */
+#endif
 #include "../alsa-kernel/core/jack.c"
