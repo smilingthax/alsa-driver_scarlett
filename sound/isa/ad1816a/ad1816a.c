1677309ce2c99fd16814d2878b86f8b1f055ccfc
Krzysztof Helt <krzysztof.h1@wp.pl>
2009-01-21 15:14:09 +0100

    ALSA: ad1816a: enable hardware timer
    Enable hardware timer with 10 usec resolution.
    
    Signed-off-by: Krzysztof Helt <krzysztof.h1@wp.pl>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- sound/isa/ad1816a/ad1816a.c
+++ sound/isa/ad1816a/ad1816a.c
@@ -156,6 +156,7 @@ static int __devinit snd_card_ad1816a_probe(int dev, struct pnp_card_link *pcard
 	struct snd_card_ad1816a *acard;
 	struct snd_ad1816a *chip;
 	struct snd_opl3 *opl3;
+	struct snd_timer *timer;
 
 	error = snd_card_create(index[dev], id[dev], THIS_MODULE,
 				sizeof(struct snd_card_ad1816a), &card);
@@ -191,6 +192,12 @@ static int __devinit snd_card_ad1816a_probe(int dev, struct pnp_card_link *pcard
 	}
 
 	if ((error = snd_ad1816a_mixer(chip)) < 0) {
+		snd_card_free(card);
+		return error;
+	}
+
+	error = snd_ad1816a_timer(chip, 0, &timer);
+	if (error < 0) {
 		snd_card_free(card);
 		return error;
 	}
