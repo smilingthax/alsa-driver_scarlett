06a4b9a84ab1465b4bcfb01a92c5e17eb736576b
Jaroslav Kysela <perex@perex.cz>
2009-04-08 18:00:01 +0200

    tea57xx: try to resolved compilation problems for 2.6.27 kernels
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- alsa/i2c/other/tea575x-tuner.c
+++ alsa/i2c/other/tea575x-tuner.c
@@ -6,9 +6,10 @@
 #endif
 #include <linux/version.h>
 
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,29)
-#include "tea575x-tuner-old.c"
-#else
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 30)
 #include "adriver.h"
-#include "../../alsa-kernel/sound/i2c/other/tea575a-tuner.c"
+#include "../../alsa-kernel/i2c/other/tea575x-tuner.c"
+#else
+#include "tea575x-tuner-old.h"
+#include "tea575x-tuner-old.c"
 #endif
