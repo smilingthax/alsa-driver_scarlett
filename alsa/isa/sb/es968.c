c7ff3cdbecdfc237fc736b30e6c6ec7927a81911
Takashi Iwai <tiwai@suse.de>
2010-05-10 09:53:21 +0200

    ALSA: Merge es968 into es1688 driver
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/isa/sb/es968.c
+++
@@ -1,8 +0,0 @@
-#include "adriver.h"
-#include "../../alsa-kernel/isa/sb/es968.c"
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,0)
-#ifndef __isapnp_now__
-#include "es968.isapnp"
-#endif
-EXPORT_NO_SYMBOLS;
-#endif
