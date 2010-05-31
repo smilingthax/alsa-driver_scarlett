14170977ab334e1d9c4b26cbc1cea048fdb0da71
Takashi Iwai <tiwai@suse.de>
2010-05-31 12:39:59 +0200

    Fix build with 2.6.35-rc1
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/pcmcia/vx/vxpocket.c
+++ alsa/pcmcia/vx/vxpocket.c
@@ -4,6 +4,8 @@
 #include "vxpocket_old.c"
 #elif LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,16)
 #include "vxpocket-2.6.16.c"
+#elif LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,34)
+#include "vxpocket-2.6.34.c"
 #else
 #include "adriver.h"
 #include "../../alsa-kernel/pcmcia/vx/vxpocket.c"
