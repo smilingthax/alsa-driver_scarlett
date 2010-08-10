f4bde4efc106e0a85930c47b389ca2982ebbb660
Takashi Iwai <tiwai@suse.de>
2010-08-10 11:42:41 +0200

    Fix typos in the previous patch
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/pcmcia/pdaudiocf/pdaudiocf_core.c
+++ alsa/pcmcia/pdaudiocf/pdaudiocf_core.c
@@ -1,5 +1,5 @@
 #define __NO_VERSION__
 #include "adriver.h"
-#include <pcmcida/cs_types.h>
+#include <pcmcia/cs_types.h>
 #include "../../alsa-kernel/pcmcia/pdaudiocf/pdaudiocf_core.c"
 EXPORT_NO_SYMBOLS;
--- alsa/pcmcia/vx/vxp_mixer.c
+++ alsa/pcmcia/vx/vxp_mixer.c
@@ -1,4 +1,4 @@
 #define __NO_VERSION__
 #include "adriver.h"
-#include <pcmcida/cs_types.h>
+#include <pcmcia/cs_types.h>
 #include "../../alsa-kernel/pcmcia/vx/vxp_mixer.c"
