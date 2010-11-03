aea7e1c6a62f42ba4249d56cf98cc38d066b0d46
Takashi Iwai <tiwai@suse.de>
2010-11-03 09:21:25 +0100

    Create a dummy pcmcia/cs.h
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/pcmcia/vx/vxpocket.h
+++ alsa/pcmcia/vx/vxpocket.h
@@ -1,7 +1,5 @@
-#if LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 35)
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 36)
 #include <pcmcia/cs.h>
-#endif
+#if LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 35)
 #include "../../alsa-kernel/pcmcia/vx/vxpocket.h"
 #elif LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 35)
 #include "vxpocket-2.6.35.h"
