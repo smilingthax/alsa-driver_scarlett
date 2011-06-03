684a5103ba9ec75531ba83de373b3db1378af87b
Takashi Iwai <tiwai@suse.de>
2011-06-03 16:49:59 +0200

    Fix build of firewire drivers with 2.6.39 kernel
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/firewire/isight.c
+++ alsa/firewire/isight.c
@@ -1,3 +1,6 @@
 #include "adriver.h"
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 39)
+#include "iso-resources-old.h"
+#endif
 #include "../alsa-kernel/firewire/isight.c"
 EXPORT_NO_SYMBOLS;
--- alsa/firewire/iso-resources.c
+++
@@ -1,3 +0,0 @@
-#define __NO_VERSION__
-#include "adriver.h"
-#include "../alsa-kernel/firewire/iso-resources.c"
