56bca927bb7a51d92bf76357872e84f20ce83e9e
Takashi Iwai <tiwai@suse.de>
2011-08-04 18:27:36 +0200

    Fix tracepoint.h wrapper
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/include/tracepoint_compat.h
+++ alsa/include/tracepoint_compat.h
@@ -2,9 +2,15 @@
 #define __TRACEPOINT_COMPAT_H
 
 #define DECLARE_EVENT_CLASS(name, proto, args, str, assign, print)
+#define TP_PROTO(x, args...) x, ##args
+#define TP_ARGS(args...)
+#define TP_STRUCT__entry(args)
+#define TP_fast_assign(args)
 #define DECLARE_TRACE(name, proto, args) \
 	static inline void trace_##name(proto) {}
+#define TRACE_EVENT(name, proto, args, str, assign, print) \
+	static inline void trace_##name(proto) {}
 #define DEFINE_EVENT(temp, name, proto, args) \
-	DECLARE_TRACE(name, proto, args)
+	static inline void trace_##name(proto) {}
 
 #endif /* __TRACEPOINT_COMPAT_H */
