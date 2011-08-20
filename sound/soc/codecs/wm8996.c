7b616d6ca765677ab3efdf7dd5f0c81d6f477f5d
Mark Brown <broonie@opensource.wolfsonmicro.com>
2011-08-20 19:28:59 +0100

    ASoC: Optimise WM8996 no interrupt path
    This occurs frequently if we are in edge triggered mode as we must poll the
    interrupt status register until we get no more interrupts so it's worth
    the effort - it means we skip writing null acknowledgements to the chip.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Liam Girdwood <lrg@ti.com>

--- sound/soc/codecs/wm8996.c
+++ sound/soc/codecs/wm8996.c
@@ -2413,6 +2413,9 @@ static irqreturn_t wm8996_irq(int irq, void *data)
 	}
 	irq_val &= ~snd_soc_read(codec, WM8996_INTERRUPT_STATUS_2_MASK);
 
+	if (!irq_val)
+		return IRQ_NONE;
+
 	snd_soc_write(codec, WM8996_INTERRUPT_STATUS_2, irq_val);
 
 	if (irq_val & (WM8996_DCS_DONE_01_EINT | WM8996_DCS_DONE_23_EINT)) {
@@ -2431,10 +2434,7 @@ static irqreturn_t wm8996_irq(int irq, void *data)
 	if (irq_val & WM8996_MICD_EINT)
 		wm8996_micd(codec);
 
-	if (irq_val)
-		return IRQ_HANDLED;
-	else
-		return IRQ_NONE;
+	return IRQ_HANDLED;
 }
 
 static irqreturn_t wm8996_edge_irq(int irq, void *data)
