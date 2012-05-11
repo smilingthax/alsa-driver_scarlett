78ab938654225dc8826259ccbb718c67c17992d7
Takashi Iwai <tiwai@suse.de>
2012-05-11 14:58:37 +0200

    Fix a typo in tea575x-tuner.c
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/i2c/other/tea575x-tuner.c
+++ alsa/i2c/other/tea575x-tuner.c
@@ -3,5 +3,5 @@
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 4, 0)
 #include "tea575x-tuner-3.3.c"
 #else
-#include "../../alsa-kernel/i2c/other/tea575-tuner.c"
+#include "../../alsa-kernel/i2c/other/tea575x-tuner.c"
 #endif
