47e97ca220169bc88fe2757de2ad8914c23e9013
Takashi Iwai <tiwai@suse.de>
2012-05-15 12:22:52 +0200

    Remove vga_default_device() hack for hda_intel.c
    This breaks the build with old kernel such as 2.6.32, since linux/vgaarb.h
    isn't protected properly for double reads...
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/pci/hda/hda_intel_hack.c
+++ alsa/pci/hda/hda_intel_hack.c
@@ -3,9 +3,6 @@
 /* workaround for the vga-switcheroo audio client handling */
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 5, 0)
 #undef CONFIG_VGA_SWITCHEROO
-#include <linux/vgaarb.h>
-#undef vga_default_device
-#define vga_default_device()	NULL
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 34)
 #define vga_switcheroo_unregister_client(pci)
 #endif
