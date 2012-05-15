6d0b59252484b6d4fd653f853334c5c202df33ad
Takashi Iwai <tiwai@suse.de>
2012-05-15 11:35:42 +0200

    Fix the build of firewire/amdtp.c with older kernels
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/firewire/amdtp.c
+++ alsa/firewire/amdtp.c
@@ -3,4 +3,7 @@
 #if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 39)
 #define fw_iso_context_queue_flush(x)
 #endif
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 4, 0)
+#define fw_iso_context_flush_completions(x)
+#endif
 #include "../alsa-kernel/firewire/amdtp.c"
