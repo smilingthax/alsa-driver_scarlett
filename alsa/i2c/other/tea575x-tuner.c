b9d926a595fbeb59383ba64f980d0a9c474f867b
Takashi Iwai <tiwai@suse.de>
2012-06-04 11:10:54 +0200

    Add a workaround to build tea575-tuner.c with 3.4 kernel
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/i2c/other/tea575x-tuner.c
+++ alsa/i2c/other/tea575x-tuner.c
@@ -3,5 +3,8 @@
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 4, 0)
 #include "tea575x-tuner-3.3.c"
 #else
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 5, 0)
+#define v4l2_disable_ioctl(x, y) /* NOP */
+#endif
 #include "../../alsa-kernel/i2c/other/tea575x-tuner.c"
 #endif
