7717342f9aecae64e3cdef6aa5fafbb87f2644e4
Jaroslav Kysela <perex@perex.cz>
2012-06-25 11:52:41 +0200

    tree merge: Update acore/jack.c to apply next merged patch
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- alsa/acore/jack.c
+++ alsa/acore/jack.c
@@ -1,7 +1,3 @@
 #define __NO_VERSION__
 #include "adriver.h"
-#include <linux/input.h>
-#ifndef SW_LINEOUT_INSERT
-#define SW_LINEOUT_INSERT	0x06  /* set = inserted */
-#endif
 #include "../alsa-kernel/core/jack.c"
