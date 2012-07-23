31a4ee2e3c1141bad08573d4692d105108bf5d6c
Takashi Iwai <tiwai@suse.de>
2012-07-23 14:50:39 +0200

    Fix the build of snd-pmac driver with older kernel
    Revert the change for machine_is_compatible()
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/ppc/burgundy.c
+++ alsa/ppc/burgundy.c
@@ -4,4 +4,7 @@
 #define CONFIG_ADB_CUDA
 #define CONFIG_ADB_PMU
 #endif
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 34)
+#define of_machine_is_compatible	machine_is_compatible
+#endif
 #include "../alsa-kernel/ppc/burgundy.c"
