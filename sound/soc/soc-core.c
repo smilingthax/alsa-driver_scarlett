728592580c6107c12b0ffbe7616f67753056b628
Peter Ujfalusi <peter.ujfalusi@ti.com>
2012-08-07 10:24:13 +0300

    ASoC: core: Set dapm->idle_bias_off for DAIs not mapped with a codec
    The idle_bias_off flag is not configured for DAIs not mapped with a codec.
    This causes the pm counter to be increased at probe time for the CPU dai
    which unbalances the pm counter handling.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -3717,6 +3717,9 @@ int snd_soc_register_dai(struct device *dev,
 		}
 	}
 
+	if (!dai->codec)
+		dai->dapm.idle_bias_off = 1;
+
 	list_add(&dai->list, &dai_list);
 
 	mutex_unlock(&client_mutex);
@@ -3804,6 +3807,9 @@ int snd_soc_register_dais(struct device *dev,
 				break;
 			}
 		}
+
+		if (!dai->codec)
+			dai->dapm.idle_bias_off = 1;
 
 		list_add(&dai->list, &dai_list);
 
