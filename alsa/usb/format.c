715ead18cf9ad776cb75c8a8d8b9fb3ae91d67b2
Takashi Iwai <tiwai@suse.de>
2013-04-05 09:15:07 +0200

    Fix build of usb/format.c for missing UAC2_FORMAT_TYPE_I_RAW_DATA
    The upstream commit 717bfb5f introduced the new definition.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/usb/format.c
+++ alsa/usb/format.c
@@ -1,3 +1,11 @@
 #define __NO_VERSION__
 #include "usbaudio.inc"
+
+#include <linux/usb.h>
+#include <linux/usb/audio.h>
+#include <linux/usb/audio-v2.h>
+#ifndef UAC2_FORMAT_TYPE_I_RAW_DATA
+#define UAC2_FORMAT_TYPE_I_RAW_DATA	(1 << 31)
+#endif
+
 #include "../alsa-kernel/usb/format.c"
